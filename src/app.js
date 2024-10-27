802b59690f3f75c509aee5d5df0f99698927b252
aasif-iqbal <aasif.iqbal3000@gmail.com>
2024-10-27 19:17:26 +0530

    add socket inside controller

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
+import http, { Server } from 'http';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { initializeSocket } from './service/socket.io.js';
+
 import dbConnection from './database/dbConnection.js';
 import bookingRouter  from './routes/bookings.route.js';
 import slotsRouter from './routes/slot.route.js';
-
 
 const app = express();
 app.use(express.json());
@@ -17,14 +17,9 @@ app.use('/api/v1', bookingRouter);
 app.use('/api/v1', slotsRouter);
 
 // Create HTTP server and attach Socket.IO
-const server = http.createServer(app);
-
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000", // Your frontend's URL
-        methods: ["GET", "POST"],
-    },
-});
+const server = http.createServer(app);  
+// Initialize Socket.IO
+const io = initializeSocket(server);
 
 // Get __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
@@ -37,34 +32,13 @@ app.get('/test', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'test.html'));
 });
 
-
-
-
-// app.post('/api/v1/book', async (req, res) => {
-    
-//     try{
-//         const { email, phone, reason, summary, date, slot } = req.body;
-//         console.log(email, phone, reason, summary, date, slot);
-
-//         // add model name here
-//         const saveData = new Booking({email, phone, reason, summary, date, slot});
-
-//         await saveData.save();
-
-//     }catch(err){
-//         console.log(err);
-//     }
-    
-// })
-
-
-
-
-
-
-
-
-
+/*
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000", // Your frontend's URL
+        methods: ["GET", "POST"],
+    },
+});
 
 // Set up Socket.IO connection
 io.on('connection', (socket) => {
@@ -83,12 +57,12 @@ io.on('connection', (socket) => {
         console.log('User disconnected:', socket.id);
     });
 });
-// Start the server with Socket.IO
+*/
 
 dbConnection().then(()=>{
     server.listen(3000, () => {
         console.log(`Server is running on port 3000`);
-    });
+    })
 }).catch((err)=>{
     console.log(err)
 });
--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -2,6 +2,7 @@ import bookings from "../models/bookings.model.js";
 import slots from "../models/slots.model.js";
 import users from "../models/users.model.js";
 
+import { getIo } from "../service/socket.io.js";
 const postBooking  = async(req, res) => {
     try {        
         const { name, phoneNo, reason, note, date, slot } = req.body;                
@@ -9,7 +10,9 @@ const postBooking  = async(req, res) => {
         let selectedSlot = slot.split('_');
         let startTime = new Date(selectedSlot[0]);
         let endTime = new Date(selectedSlot[1]);
-                
+        
+        const io = getIo();
+
         const pipeline = [
             {
                 $match: {                    
@@ -45,16 +48,20 @@ const postBooking  = async(req, res) => {
         
         let _bookings = new bookings(bookingDetails);
         // after saving user and slot then save booking with userId and slotId
-        Promise.all([await _users.save(), await _bookings.save()])
-            .then((response) => {             
+        const createBooking = Promise.all([await _users.save(), await _bookings.save()])
+            .then(async (response) => {    
+                // fetch booking details of current users with time slot and send data to frontend to update view.
+                await bookings.find({});
+                io.emit('update', {'asd':'daskads'})  
                 console.log(response);      
             }).catch((error)=>{
                 console.log(`error:${error}`);
-            });                                
+            }); 
+
         return res.status(201).send({
             status:"success",
             message:"Booking has made",
-            data: {}
+            data: {createBooking}
         });                                 
     } catch (error) {
         console.log(error);
